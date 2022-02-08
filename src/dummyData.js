2c269c4aa3500cbe4f426897341e532ee2244ef8
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-02-08 19:55:22 +0600

    new sectoin added

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 3000,
+    },
+    {
+      name: 'Mar',
+      "Active User": 5000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 4000,
+    },
+    {
+      name: 'May',
+      "Active User": 2000,
+    },
+    {
+      name: 'June',
+      "Active User": 4000,
+    },
+    {
+      name: 'July',
+      "Active User": 6000,
+    },
+    {
+      name: 'Aug',
+      "Active User": 4000,
+    },
+    {
+      name: 'Sept',
+      "Active User": 3000,
+    },
+    {
+      name: 'Oct',
+      "Active User": 1000,
+    },
+    {
+      name: 'Nov',
+      "Active User": 4000,
+    },
+    {
+      name: 'Dec',
+      "Active User": 3000,
+    },
+   
+  ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import "./home.css";
+import {userData} from '../../dummyData';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 function Home() {
   return ( 
    <div className="home">
        <FeaturedInfo/>
-       <Chart/>
+       <Chart data={userData} title="User Analytics" grid dataKey = "Active User"/>
+       <div className="homeWidgets">
+         <WidgetSm/>
+         <WidgetLg/>
+       </div>
    </div>
    );
 }
